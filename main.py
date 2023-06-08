373a7b36ee33b460a801a8be36c201d8b21852fb
Jack Krupienski <krupienskij@wit.edu>
2023-06-08 09:49:17 -0400

    NO change

--- main.py
+++ main.py
@@ -1,74 +1,73 @@
-import kivy.properties
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.graphics import Color, Rectangle
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.button import Button
-from kivy.config import Config
-from kivy.uix.image import Image, AsyncImage
-fullscreen = kivy.properties.OptionProperty('auto')
+from tkinter import *
+import tkinter as tk
+
+import sqlite3
+
+# con = sqlite3.connect("assignment3.db")
+# cur = con.cursor()
+# cur.execute("INSERT INTO STUDENT VALUES(101,'Kaleb','Pelletier','COMP_ENG','pelletierk3@wit.edu',2024);")
+# con.commit()
+# con.close()
 
 
-class RootWidget(FloatLayout):
+window = Tk()
 
-    def __init__(self, **kwargs):
-        # make sure we aren't overriding any important functionality
-        super(RootWidget, self).__init__(**kwargs)
+# mainWindow = Toplevel()
+# mainWindow.withdraw() #Hides window
 
-        # Background Poker Image
-        self.add_widget(
-            AsyncImage(
-                source='https://thumbs.dreamstime.com/b/cards-green-felt-casino-table-background-cards-green-felt-casino-table-background-template-copy-space-center-138053426.jpg')
-        )
-        # Buttons to get to games
-        self.add_widget(
-            Button(
-                text="Blackjack",
-                size_hint=(.30, .20),
-                pos_hint={'left_x': .5, 'left_y': .10}))
-        self.add_widget(
-            Button(
-                text="Roulette",
-                size_hint=(.30, .20),
-                pos_hint={'left_x': .5, 'center_y': .30}))
-        self.add_widget(
-            Button(
-                text="Slots",
-                size_hint=(.30, .20),
-                pos_hint={'left_x': .5, 'center_y': .50}))
-        self.add_widget(
-            Button(
-                text="Solitaire",
-                size_hint=(.30, .20),
-                pos_hint={'left_x': .5, 'center_y': .70}))
-        self.add_widget(
-            Button(
-                text="Bacarratt",
-                size_hint=(.30, .20),
-                pos_hint={'left_x': .5, 'center_y': .90}))
-        self.add_widget(
-            Button(
-                text="Financials",
-                size_hint=(.30, .20),
-                pos_hint={'x': .7,'y': .8}))
-
-class MainApp(App):
-
-    def build(self):
-        self.root = root = RootWidget()
-
-        root.bind(size=self._update_rect, pos=self._update_rect)
-
-        with root.canvas.before:
-            Color(0, 0, 0, 1)
-            self.rect = Rectangle(size=root.size, pos=root.pos)
-        return root
-
-    def _update_rect(self, instance, value):
-        self.rect.pos = instance.pos
-        self.rect.size = instance.size
+input1 = tk.Entry(window)
+input2 = tk.Entry(window)
 
 
+def createNew():
+    mainWindow = Toplevel()
+    mainWindow.geometry("500x500")
+    tk.Label(mainWindow, text="Create New User").grid(row=0)
+    tk.Label(mainWindow, text="First Name").grid(row=1)
+    tk.Label(mainWindow, text="Last Name").grid(row=2)
+    tk.Label(mainWindow, text="UserName").grid(row=3)
 
-if __name__ == '__main__':
-    MainApp().run()
+
+def getLoginInfo():
+    mainWindow = Toplevel()
+    mainWindow.geometry("800x800")
+
+    # loop through the data tables to check if they exist
+    userN = input1.get()
+    if (userN == "1"):
+        mainWindow.title("Player")
+    if (userN == "2"):
+        mainWindow.title("Manager")
+
+    userN = ""
+    mainWindow.deiconify()  # Shows window
+    # mainWindow.mainloop()
+
+    # tk.Label(window, text=userN).grid(row=4)
+
+
+def main():
+    window.title("WIT Casino")
+    window.geometry("500x500")
+
+    tk.Label(window, text="WIT Casino").grid(row=0)
+    tk.Label(window, text="Username").grid(row=1)
+    tk.Label(window, text="Password").grid(row=2)
+
+    Loginbtn = Button(window, text="Log In", command=getLoginInfo)
+    createNewUser = Button(window, text="Create New", command=createNew)
+
+    # username = input1.get()
+    # password = input2.get()
+    # if(username != "" ):
+    #     mainWindow = Tk()
+
+    input1.grid(row=1, column=1)
+    input2.grid(row=2, column=1)
+    Loginbtn.grid(row=3)
+    createNewUser.grid(row=3, column=1)
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
