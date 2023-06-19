8fb99cdbce3c3c26620919ece192a3b3a0b3a3da
pelletierk20 <pelletierk3@wit.edu>
2023-06-19 15:58:54 -0400

    Database basic comms

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from tkinter import *
 import tkinter as tk
 
 import sqlite3 
-con = sqlite3.connect("Casino.db")
+import os.path
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "Casino.db")
+con = sqlite3.connect(db_path)
 cur = con.cursor()
 #cur.execute("INSERT INTO STUDENT VALUES(101,'Kaleb','Pelletier','COMP_ENG','pelletierk3@wit.edu',2024);")
 class User:
@@ -19,21 +23,24 @@ class User:
         return self.user
     
 
-    def __del__(self):
-        print(self.name + " is deleted")
+    # def __del__(self):
+    #     print(self.name + " is deleted")
         #del obj
 
 class Player(User):
-    def __init__(self,pMoneyMade,pMoneyLost,currGame,pWin,pLoss,pCredit):
+    
+    def __init__(self,fName,lName,uName,pMoneyMade,pMoneyLost,currGame,pWin,pLoss):
+        User.__init__(self,fName,lName,uName)
         self.moneyMade = pMoneyMade
         self.moneyLost = pMoneyLost
         self.currentGame = currGame
         self.winCount = pWin
         self.lossCount = pLoss
-        self.creditAmount = pCredit
+        #self.creditAmount = pCredit
+        
         cur.execute("INSERT INTO Player VALUES('"+self.user+"','"+self.name+"','"+self.last+"',"+
                     self.moneyMade+","+self.moneyLost+",'"+self.currentGame+"',"+self.winCount+","+
-                    self.lossCount+","+self.creditAmount+");")
+                    self.lossCount+");")#","+self.creditAmount+");")
 
 
 
@@ -69,7 +76,7 @@ class Manager(User):
 
 
 my_w = tk.Tk()
-my_w.geometry("200x200")  # Size of the window
+my_w.geometry("250x250")  # Size of the window
 my_w.title("WIT CASINO")  # Adding a title
 
 # create one lebel
@@ -96,25 +103,28 @@ b2 = tk.Button(my_w, text='Login',
                command=lambda:my_login())
 b2.grid(row=5,column=1)
 
-window = Tk()
-mainWindow = Tk()
-mainWindow.withdraw()
+#window = Tk()
+#mainWindow = Tk()
+# mainWindow.withdraw()
 
-input1= tk.Entry(window)
-input2 = tk.Entry(window)
+# input1= tk.Entry(window)
+# input2 = tk.Entry(window)
 
 def my_login():
     print("doing the login stuff")
 
 def create(first,last,user):
     print("database creating things")
-    print(first)
-    print(last)
-    print(user)
+    table = cur.fetchall()
+    print(table)
+    p = Player(first,last,user,"0","0","0","0","0")
+    con.commit()
+    
+
 def my_open():
     my_w_child=Toplevel(my_w) # Child window
-    my_w_child.geometry("200x200")  # Size of the window
-    my_w_child.title("www.plus2net.com")
+    my_w_child.geometry("250x250")  # Size of the window
+    my_w_child.title("New User Screen")
 
     inFirstName = tk.Entry(my_w_child)
     inLastName = tk.Entry(my_w_child)
@@ -124,20 +134,21 @@ def my_open():
     inLastName.grid(row=2, column=1)
     inUserName.grid(row=3, column=1)
 
-    firstN = inFirstName.get()  # Input for create new
-    lastN = inLastName.get()
-    userN = inUserName.get()
+    # firstN = inFirstName.get()  # Input for create new
+    # lastN = inLastName.get()
+    # userN = inUserName.get()
+    
     my_str1 = tk.StringVar()
 
     l1 = tk.Label(my_w_child,  textvariable=my_str1 )
     l1.grid(row=1,column=2)
-    my_str1.set("USERNAME")
-    l2 = tk.Label(my_w_child, text="PASSWORD")
+    my_str1.set("User Name")
+    l2 = tk.Label(my_w_child, text="Password")
     l2.grid(row=2,column=2)
-    l3 = tk.Label(my_w_child, text= "USERNAME")
+    l3 = tk.Label(my_w_child, text= "User Name")
     l3.grid(row=3,column=2)
     b3 = tk.Button(my_w_child, text=' Create ',
-                   command= lambda:[my_w_child.destroy(),create(firstN,lastN,userN)])
+                   command= lambda:[create(inFirstName.get(),inLastName.get(),inUserName.get()),my_w_child.destroy()])
     b3.grid(row=4,column=1)
 
 
@@ -147,6 +158,7 @@ def my_open():
 
 def main():
     my_w.mainloop()
+
 
     
 
