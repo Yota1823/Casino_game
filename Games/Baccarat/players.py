81ba8936b713add8d5b388b824b5ecf4fcd558ad
Zainsidd3 <133975976+Zainsidd3@users.noreply.github.com>
2023-07-22 22:39:01 -0400

    fix baccarat functionality

--- Games/Baccarat/players.py
+++ Games/Baccarat/players.py
@@ -4,9 +4,9 @@ class Player:
     Args:
         balance: int, the initial balance of the player.
 
-    Attributes:
-        pid: int, sequential id number of the player.
-        balance: int, the balance of the player.
+    Atributes:
+        pid: int, sequencial id number of the playeri.
+        balance: int, the balance of player.
         hand_bet: str, the hand in which the player is betting.
         amount_bet: int, the amount of a bet.
 
@@ -14,28 +14,18 @@ class Player:
         TypeError: if the balance is not an integer.
         ValueError: if the balance is not positive.
     """
-    _pid = 1  # Class variable to assign sequential player IDs
-    def __init__(self, balance, pLastName, pFirstName, pUserName, pMoneyMade, pMoneyLost, pLost, pWin):
-        self._balance = balance
-        self._hand_bet = None
-        self._amount_bet = None
-        self.pLastName = pLastName
-        self.pFirstName = pFirstName
-        self.pUserName = pUserName
-        self.pMoneyMade = pMoneyMade
-        self.pMoneyLost = pMoneyLost
-        self.pLost = pLost
-        self.pWin = pWin
-        self.currGame = 'Baccarat'
+    _pid = 1
+
+    def __init__(self, balance):
         if not isinstance(balance, int):
             raise TypeError('Balance must be an integer.')
         elif balance < 1:
             raise ValueError('Balance must be positive.')
-        self._pid = Player._pid  # Assign the player ID
+        self._pid = Player._pid
         self._balance = balance
         self._hand_bet = None
         self._amount_bet = 0
-        Player._pid += 1  # Increment the player ID for the next instance
+        Player._pid += 1
 
     @property
     def pid(self):
@@ -52,14 +42,14 @@ class Player:
         """Get the hand on which the bet was made.
 
         Raises:
-            ValueError: When setting if the value is neither player,
-                banker, or tie.
+            ValueError: When setting if the value is neither punto,
+                banco or tie.
         """
         return self._hand_bet
 
     @hand_bet.setter
     def hand_bet(self, hand):
-        if hand not in ['player', 'banker', 'tie']:
+        if hand not in ['punto', 'banco', 'tie']:
             raise ValueError('Invalid hand.')
         self._hand_bet = hand
 
@@ -68,8 +58,8 @@ class Player:
         """Get the amount of a bet.
 
         Raises:
-            TypeError: When setting if the amount is not an integer.
-            ValueError: When setting if the amount exceeds the
+            TypeError: When setting if the amount is a integer.
+            ValueError: When setting if the amount exceed the
                 available balance.
         """
         return self._amount_bet
@@ -77,7 +67,7 @@ class Player:
     @amount_bet.setter
     def amount_bet(self, amount):
         if not isinstance(amount, int):
-            raise TypeError('Amount must be an integer.')
+            raise TypeError('Amount must be a integer.')
         if amount < 1:
             raise ValueError('Amount must be positive.')
         if amount > self._balance:
@@ -88,26 +78,23 @@ class Player:
         """Checks if the current bet is valid.
 
         Returns:
-            bool: True if the bet is valid, False otherwise.
+            bol, True if the bet is valid, False otherwise.
         """
-        if self._hand_bet not in ['player', 'banker', 'tie'] or self._amount_bet <= 0:
+        if self._hand_bet not in ['punto', 'banco', 'tie'] or self._amount_bet <= 0:
             return False
         return True
 
     def win(self):
         """Perform the necessary actions upon a player win: adds the winnings
-        to the balance according to the bet and resets the bet.
+        to the balance according the bet and resets the bet.
 
         Raises:
             InvalidBet: If the player does not have a valid bet.
         """
         if self.is_valid_bet():
-            if self._hand_bet == 'player':
+            if self._hand_bet == 'punto':
                 self._balance += int(self._amount_bet * 1)
-                #add money made 
-                #add the pWin
-
-            elif self._hand_bet == 'banker':
+            elif self._hand_bet == 'banco':
                 self._balance += int(self._amount_bet * 0.95)
             elif self._hand_bet == 'tie':
                 self._balance += int(self._amount_bet * 8)
@@ -117,14 +104,12 @@ class Player:
             raise InvalidBet('Player does not have a valid bet.')
 
     def lose(self):
-        """Perform the necessary action upon a player loss: resets the bet.
+        """Performs the necessary action upon a player lose: resets the bet.
 
         Raises:
             InvalidBet: If the player does not have a valid bet.
         """
         if self.is_valid_bet():
-            # add player money lost
-
             self._balance -= self._amount_bet
             self._hand_bet = None
             self._amount_bet = 0
@@ -138,17 +123,12 @@ class Player:
         return f'Player({self._balance})'
 
     def __str__(self):
-        """Return a string separated by new lines with the id, amount, and bet
+        """Return a string separated by new lines with the id, amount and bet
         of the player in case there is a valid one.
         """
         bet = f'Hand bet: {self._hand_bet}, Amount bet: {self._amount_bet}'
         no_bet = 'No bet'
         return f'Player: {self._pid}, Balance: {self._balance}, {bet if self.is_valid_bet() else no_bet}.'
 
-
 class InvalidBet(Exception):
-    pass
-
-
-class GameError(Exception):
     pass
